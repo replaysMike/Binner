d03d382131d5fc35a87fe10a7b02fe7368a8c15d
Michael Brown <mikeb@360replays.com>
2019-10-12 14:18:55 -0700

    Initial project commit

---
+++ Binner/Binner.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Binner.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Binner/Binner.Web/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Binner.Web
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
