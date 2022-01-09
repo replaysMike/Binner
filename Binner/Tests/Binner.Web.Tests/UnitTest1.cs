a6a6540308c94079af11909cb99b5c3c6e78bf23
Michael Brown <mikeb@360replays.com>
2022-01-08 18:10:01 -0800

    scaffolded test projects and restructed the project folders

---
+++ Binner/Tests/Binner.Web.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Binner.Web.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
