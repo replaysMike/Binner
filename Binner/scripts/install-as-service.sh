9e0699749fecf0e00f58b685db2c8b0b28d0e08f
Michael Brown <mikeb@360replays.com>
2022-01-24 19:51:02 -0800

    adds execute permission when installing the binner service on unix environments. it's unfortunate that +x permissions on windows are nearly impossible to maintain via tar. Only way to fix this would be to transition the AppVeyor build environment over to a unix vm.

--- Binner/scripts/install-as-service.sh
+++ Binner/scripts/install-as-service.sh
@@ -2,6 +2,7 @@
 
 is_user_root () { [ "$(id -u)" -eq 0 ]; }
 if is_user_root; then
+	chmod +x ./Binner.service
 	sed --expression "s#@INSTALLPATH@#${PWD}#g" Binner.service.systemctl.template > /etc/systemd/system/Binner.service
 	systemctl daemon-reload
 	systemctl enable Binner.service
