0211afceddf6ec01dc1f2377b4e2c14ad55f4119
Michael Brown <mikeb@360replays.com>
2022-01-24 19:16:19 -0800

    added scripts to install Binner as a service on unix platforms using sysctl

---
+++ Binner/scripts/install-as-service.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+is_user_root () { [ "$(id -u)" -eq 0 ]; }
+if is_user_root; then
+	sed --expression "s#@INSTALLPATH@#${PWD}#g" Binner.service.systemctl.template > /etc/systemd/system/Binner.service
+	systemctl daemon-reload
+	systemctl enable Binner.service
+	systemctl start Binner.service
+	echo "Binner.Web is now running - http://localhost:8090"
+	exit 0
+else
+	echo "This script must be run as root user using sudo.";
+	exit 1
+fi
