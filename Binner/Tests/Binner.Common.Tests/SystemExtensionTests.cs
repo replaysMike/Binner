e18416de607dfd2382c76cfa6a27d7cf46554460
Michael Brown <mikeb@360replays.com>
2023-03-10 14:38:59 -0800

    Fixed #111 properly this time - detects currency culture and should be fairly reliable at getting the proper value from the Mouser api.

---
+++ Binner/Tests/Binner.Common.Tests/SystemExtensionTests.cs
@@ -0,0 +1,33 @@
+using Binner.Common.Extensions;
+using NUnit.Framework;
+
+namespace Binner.Common.Tests
+{
+    [TestFixture]
+    public class SystemExtensionTests
+    {
+        [Test]
+        public void Should_ParseDollarCurrency()
+        {
+            var test = $"$5.50";
+            var value = test.FromCurrency();
+            Assert.That(value, Is.EqualTo(5.50));
+        }
+
+        [Test]
+        public void Should_ParseEuroCurrency()
+        {
+            var test = $"€5,50";
+            var value = test.FromCurrency();
+            Assert.That(value, Is.EqualTo(5.50));
+        }
+
+        [Test]
+        public void Should_ParseNoCurrencySymbol()
+        {
+            var test = $"5.50";
+            var value = test.FromCurrency();
+            Assert.That(value, Is.EqualTo(5.50));
+        }
+    }
+}
