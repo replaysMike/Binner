051930472510758df75cb52313e8a82abf862691
Michael Brown <mikeb@360replays.com>
2023-03-29 17:57:25 -0700

    Fixes some issues with Bom producing parts - better support for preventing boards from being produced if not enough stock

--- Binner/Binner.Web/ClientApp/src/common/bomTools.js
+++ Binner/Binner.Web/ClientApp/src/common/bomTools.js
@@ -11,12 +11,16 @@ export const getProjectColor = (ProjectColors, project) => {
 }
 
 /**
- * Get a count of parts that are in stock produce a single PCB by quantity needed
+ * Get a list of parts that are in stock produce a single PCB by quantity needed
  * @param {array} parts Parts array from BOM list
+ * @param {object} pcb Pcb to count, null to get unassociated count
  * @returns 
  */
-export const getInStockPartsCount = (parts) => {
-	return getInStockParts(parts).length;
+export const getInStockParts = (parts, pcb) => {
+	if (parts === undefined) return 0;
+	if (pcb)
+		return _.filter(parts, (s) => s.pcbId === pcb.pcbId && (s.part?.quantity || s.quantityAvailable || 0) >= s.quantity);
+	return _.filter(parts, (s) => s.pcbId === null && (s.part?.quantity || s.quantityAvailable || 0) >= s.quantity);
 };
 
 /**
@@ -24,18 +28,22 @@ export const getInStockPartsCount = (parts) => {
  * @param {array} parts Parts array from BOM list
  * @returns 
  */
-export const getInStockParts = (parts) => {
+export const getTotalInStockParts = (parts) => {
 	if (parts === undefined) return 0;
 	return _.filter(parts, (s) => (s.part?.quantity || s.quantityAvailable || 0) >= s.quantity);
 };
 
 /**
- * Get a count of parts that are out of stock produce a single PCB by quantity needed
+ * Get a list of parts that are out of stock produce a single PCB by quantity needed
  * @param {array} parts Parts array from BOM list
+ * @param {object} pcb Pcb to count, null to get unassociated count
  * @returns 
  */
-export const getOutOfStockPartsCount = (parts) => {
-	return getOutOfStockParts(parts).length;
+export const getOutOfStockParts = (parts, pcb) => {
+	if (parts === undefined) return 0;
+	if (pcb)
+		return _.filter(parts, (s) => s.pcbId === pcb.pcbId && (s.part?.quantity || s.quantityAvailable || 0) < s.quantity);
+	return _.filter(parts, (s) => s.pcbId === null && (s.part?.quantity || s.quantityAvailable || 0) < s.quantity);
 };
 
 /**
@@ -43,9 +51,36 @@ export const getOutOfStockPartsCount = (parts) => {
  * @param {array} parts Parts array from BOM list
  * @returns 
  */
-export const getOutOfStockParts = (parts) => {
+export const getTotalOutOfStockParts = (parts) => {
 	if (parts === undefined) return 0;
 	return _.filter(parts, (s) => (s.part?.quantity || s.quantityAvailable || 0) < s.quantity);
+};
+
+/**
+ * Compute the total number of PCBs that can be produced based on BOM list inventory not assigned to a PCB
+ * @param {array} parts Parts array from BOM list
+ * @returns The count of how many PCBs can be produced
+ */
+export const getProducibleUnassociatedCount = (parts) => {
+	if (parts === undefined) return 0;
+
+	// deep clone the parts array
+	const partsConsumed = parts.filter(x => x.pcbId === null).map(x => ({ quantity: x.quantity, part: { quantity: x.part?.quantity || x.quantityAvailable || 0 }}));
+	let pcbsProduced = 0;
+	let pcbsExceeded = false;
+	for(let virtualPcb = 0; virtualPcb < 10000; virtualPcb++){
+		for(let i = 0; i < partsConsumed.length; i++) {
+			partsConsumed[i].part.quantity -= partsConsumed[i].quantity;
+			if (partsConsumed[i].part.quantity < 0) {
+				pcbsExceeded = true;
+				break;
+			}
+		}
+		if (pcbsExceeded)
+			break;
+		pcbsProduced++;
+	}
+	return pcbsProduced;
 };
 
 /**
@@ -56,41 +91,35 @@ export const getOutOfStockParts = (parts) => {
  */
 export const getProduciblePcbCount = (parts, pcb) => {
 	if (parts === undefined) return { count: 0, limitingPcb: -1 };
-	const inStock = getInStockPartsCount(parts);
-	const outOfStock = getOutOfStockPartsCount(parts);
-	if (outOfStock > 0) {
-		return { count: 0, limitingPcb: -1 };
-	}
-	if (inStock > 0) {
-		// deep clone the parts array
-		const partsConsumed = parts.map(x => ({ quantity: x.quantity, part: { quantity: x.part?.quantity || x.quantityAvailable || 0, pcbId: x.pcbId }}));
-		let pcbsProduced = 0;
-		let pcbsExceeded = false;
-		let limitingPcb = -1;
-		for(let pcb = 0; pcb < 10000; pcb++){
-			for(let i = 0; i < partsConsumed.length; i++) {
-				const pcbId = partsConsumed[i].part.pcbId;
-				// if a pcb is provided, take into consideration the pcb quantity as well.
-				// if the pcb has a quantity > 1, it acts as a multiplier for BOMs that produce multiples of a single PCB
-				if (pcb && pcb.pcbId > 0) {
-					let pcbQuantity = pcb.quantity;
-					// a value of 0 is invalid
-					if (pcbQuantity <= 0) pcbQuantity = 1;
-					partsConsumed[i].part.quantity -= (partsConsumed[i].quantity * pcbQuantity);
-				} else {
-					partsConsumed[i].part.quantity -= partsConsumed[i].quantity;
-				}
-				if (partsConsumed[i].part.quantity < 0) {
-					pcbsExceeded = true;
-					limitingPcb = pcbId;
-					break;
-				}
+
+	// deep clone the parts array
+	const pcbParts = pcb && pcb.pcbId > 0 ? _.filter(parts, p => p.pcbId === pcb.pcbId) : parts;
+	const partsConsumed = pcbParts.map(x => ({ quantity: x.quantity, part: { quantity: x.part?.quantity || x.quantityAvailable || 0, pcbId: x.pcbId }}));
+	let pcbsProduced = 0;
+	let pcbsExceeded = false;
+	let limitingPcb = -1;
+	for(let virtualPcb = 0; virtualPcb < 10000; virtualPcb++){
+		for(let i = 0; i < partsConsumed.length; i++) {
+			const pcbId = partsConsumed[i].part.pcbId;
+			// if a pcb is provided, take into consideration the pcb quantity as well.
+			// if the pcb has a quantity > 1, it acts as a multiplier for BOMs that produce multiples of a single PCB
+			if (pcb && pcbId > 0) {
+				let pcbQuantity = pcb.quantity;
+				// a value of 0 is invalid
+				if (pcbQuantity <= 0) pcbQuantity = 1;
+				partsConsumed[i].part.quantity -= (partsConsumed[i].quantity * pcbQuantity);
+			} else {
+				partsConsumed[i].part.quantity -= partsConsumed[i].quantity;
 			}
-			if (pcbsExceeded)
+			if (partsConsumed[i].part.quantity < 0) {
+				pcbsExceeded = true;
+				limitingPcb = pcbId;
 				break;
-			pcbsProduced++;
+			}
 		}
-		return { count: pcbsProduced, limitingPcb };
+		if (pcbsExceeded)
+			break;
+		pcbsProduced++;
 	}
-	return { count: 0, limitingPcb: -1 };
+	return { count: pcbsProduced, limitingPcb };
 };
