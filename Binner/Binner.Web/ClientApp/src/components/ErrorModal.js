dcf8caf52fa78f927923c5a53fc6e2d4dc51a247
Michael Brown <mbrown@provantagex.com>
2023-04-05 12:51:33 -0700

    more translations

--- Binner/Binner.Web/ClientApp/src/components/ErrorModal.js
+++ Binner/Binner.Web/ClientApp/src/components/ErrorModal.js
@@ -1,41 +1,37 @@
 
 import { Modal } from 'semantic-ui-react';
-import React, { Component } from 'react';
+import { useTranslation } from 'react-i18next';
+import React from 'react';
 import { ErrorContext } from '../common/ErrorContext';
 
-export default class ErrorModal extends Component {
-	static contextType = ErrorContext;
-	constructor(props) {
-		super(props);
-    	this.handleCloseErrorModal = this.handleCloseErrorModal.bind(this);
-	}
+export function ErrorModal (props) {
+	const { t } = useTranslation();
+	ErrorModal.contextType = ErrorContext;
 
-  	handleCloseErrorModal() {
+  const handleCloseErrorModal = () => {
 		window.showErrorWindow();
-	}
+	};
 
-	hasErrorMessage(message){
+	const hasErrorMessage = (message) => {
 		return message !== undefined && message.length > 0;
-	}
+	};
 
-	render() {
-		return (
-		<ErrorContext.Consumer>
-			{({modalTitle, url, header, errorMessage, stackTrace}) => (
-			<Modal centered open={this.hasErrorMessage(errorMessage)} onClose={this.handleCloseErrorModal}>
-			<Modal.Header>{modalTitle}</Modal.Header>
-			<Modal.Content scrolling dimmer='blurring'>
-				<Modal.Description>
-				{header && <h1>{header}</h1>}
-				{errorMessage && <h3>{errorMessage}</h3>}
-				<hr/>
-				{url && <h2>Api Endpoint: {url}</h2>}
-				{stackTrace && <p>{stackTrace}</p>}
-				</Modal.Description>
-			</Modal.Content>
-			</Modal>
-			)}
-		</ErrorContext.Consumer>);
-	}
-};
+	return (
+	<ErrorContext.Consumer>
+		{({modalTitle, url, header, errorMessage, stackTrace}) => (
+		<Modal centered open={hasErrorMessage(errorMessage)} onClose={handleCloseErrorModal}>
+		<Modal.Header>{modalTitle}</Modal.Header>
+		<Modal.Content scrolling dimmer='blurring'>
+			<Modal.Description>
+			{header && <h1>{header}</h1>}
+			{errorMessage && <h3>{errorMessage}</h3>}
+			<hr/>
+			{url && <h2>{t('label.apiEndpoint', "Api Endpoint")}: {url}</h2>}
+			{stackTrace && <p>{stackTrace}</p>}
+			</Modal.Description>
+		</Modal.Content>
+		</Modal>
+		)}
+	</ErrorContext.Consumer>);
+}
 ErrorModal.contextType = ErrorContext;
--- Binner/Binner.Web/ClientApp/src/components/FormError.js
+++ Binner/Binner.Web/ClientApp/src/components/FormError.js
@@ -1,10 +1,12 @@
 import { Icon, Label } from "semantic-ui-react";
+import { useTranslation } from 'react-i18next';
 
 // form error component
 export const FormError = (errorMessage) => {
+	const { t } = useTranslation();
 	if (errorMessage && errorMessage.length > 0)
 		return (<div>
-		<Label color="red"><Icon name="warning sign" /> Error: {errorMessage}</Label>
+		<Label color="red"><Icon name="warning sign" /> {t('label.error', "Error")}: {errorMessage}</Label>
 	</div>);
 	
 	return ("");
