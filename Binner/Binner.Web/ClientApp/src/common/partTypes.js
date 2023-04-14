066c8a5925cd431c320b5d2dd87c2e85952b9391
Michael Brown <mikeb@360replays.com>
2023-04-14 15:25:26 -0700

    added ability to rename a part type

--- Binner/Binner.Web/ClientApp/src/common/partTypes.js
+++ Binner/Binner.Web/ClientApp/src/common/partTypes.js
@@ -31,42 +31,59 @@ export const getIcon = (name, parentName) => {
 const getIconForType = (name) => {
   switch (name) {
     case "Cable":
+		case "Cables":
       return CableIcon;
     case "Capacitor":
+		case "Capacitors":
       return CapacitorIcon;
 		case "Connector":
+		case "Connectors":
       return ConnectorIcon;
 		case "Crystal":
+		case "Crystals":
       return CrystalIcon;
     case "Diode":
+		case "Diodes":
       return DiodeIcon;
 		case "Hardware":
 			return HardwareIcon;
     case "IC":
+		case "ICs":
       return ICIcon;
 		case "Inductor":
+		case "Inductors":
       return InductorIcon;
 		case "Kit":
+		case "Kits":
 			return KitIcon;
 		case "LED":
+		case "LEDs":
       return LEDIcon;
 		case "Module":
+		case "Modules":
       return ModuleIcon;
 		case "Relay":
+		case "Relays":
 			return RelayIcon;
     case "Resistor":
+		case "Resistors":
       return ResistorIcon;
 		case "MOSFET":
+		case "MOSFETs":
 		case "TRIAC":
 		case "SCR":
       return SCRIcon;
 		case "Sensor":
+		case "Sensors":
       return SensorIcon;
 		case "Switch":
+		case "Switches":
       return SwitchIcon;
 		case "Transformer":
+		case "Transformers":
       return TransformerIcon;
 		case "Transistor":
+		case "Transistors":
       return TransistorIcon;
     default:
       return null;
--- Binner/Library/Binner.Common/MappingProfiles/ModelCommon/PartTypeProfile.cs
+++ Binner/Library/Binner.Common/MappingProfiles/ModelCommon/PartTypeProfile.cs
@@ -17,6 +17,18 @@ namespace Binner.Common.MappingProfiles.ModelCommon
                 .ForMember(x => x.UserId, options => options.MapFrom(x => x.UserId))
                 ;
 
+            CreateMap<PartType, DataModel.PartType>(MemberList.None)
+                .ForMember(x => x.ParentPartTypeId, options => options.MapFrom(x => x.ParentPartTypeId))
+                .ForMember(x => x.PartTypeId, options => options.MapFrom(x => x.PartTypeId))
+                .ForMember(x => x.Name, options => options.MapFrom(x => x.Name))
+                .ForMember(x => x.UserId, options => options.MapFrom(x => x.UserId))
+                .ForMember(x => x.DateCreatedUtc, options => options.Ignore())
+                .ForMember(x => x.DateModifiedUtc, options => options.Ignore())
+                .ForMember(x => x.User, options => options.Ignore())
+                .ForMember(x => x.ParentPartType, options => options.Ignore())
+                .ForMember(x => x.Parts, options => options.Ignore())
+                ;
+
             CreateMap<DataModel.PartType, PartTypeResponse>()
                 .ForMember(x => x.DateCreatedUtc, options => options.MapFrom(x => x.DateCreatedUtc))
                 .ForMember(x => x.ParentPartTypeId, options => options.MapFrom(x => x.ParentPartTypeId))
