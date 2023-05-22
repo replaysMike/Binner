9e0ef8a12606dc73ce7744ab3de3b318a69157cb
Michael Brown <mikeb@360replays.com>
2023-05-21 18:00:25 -0700

    adds some missing translations. adds popup to explain Maker subscription is required to access feature

--- Binner/Binner.Web/ClientApp/src/pages/admin/Home.js
+++ Binner/Binner.Web/ClientApp/src/pages/admin/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useTranslation, Trans } from "react-i18next";
-import { Segment, Breadcrumb, Icon } from "semantic-ui-react";
+import { Segment, Breadcrumb, Popup } from "semantic-ui-react";
 import { FormHeader } from "../../components/FormHeader";
 
 export const Admin = () => {
@@ -37,10 +37,18 @@ export const Admin = () => {
             <h1><span>{t('page.admin.systemInfo.title', "System Information")}</span></h1>
               <p>{t('page.admin.systemInfo.baseDescription', "Get information on your Binner installation.")}</p>
             </li>
-            <li /*onClick={() => navigate("/admin/updateParts")}*/ className="disabled">
+            <Popup 
+              hoverable
+              content={<p>
+                <Trans i18nKey="popup.makerSubscriptionRequired">
+                  Requires a Maker level subscription. <a href="https://binner.io/signup" target="_blank" rel="noreferrer">Click here</a> for details.
+                </Trans></p>} 
+              trigger={<li /*onClick={() => navigate("/admin/updateParts")}*/ className="disabled">
               <h1 className="maker"><span>Update Part Metadata</span></h1>
               <p>Refresh information from external APIs and choose which fields you would like to update.</p>
-						</li>
+						</li>} 
+            />
+            
 						{/*<li onClick={() => navigate("/admin/activateLicense")}>
               <h1><span>Activate License</span></h1>
               <p>If you have a paid subscription on <a href="https://binner.io" target="_blank" rel="noreferrer">Binner Cloud</a>, activate your license key to gain licensed features in your Binner installation.</p>
--- Binner/Binner.Web/ClientApp/src/pages/printing/Home.js
+++ Binner/Binner.Web/ClientApp/src/pages/printing/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useTranslation, Trans } from "react-i18next";
-import { Segment, Breadcrumb, Icon } from "semantic-ui-react";
+import { Segment, Breadcrumb, Icon, Popup } from "semantic-ui-react";
 import { FormHeader } from "../../components/FormHeader";
 
 export const Printing = () => {
@@ -33,10 +33,17 @@ export const Printing = () => {
             <h1><span>{t('page.printing.printLabels.title', "Print Custom Labels")}</span></h1>
               <p>{t('page.printing.printLabels.baseDescription', "Print custom labels for any task.")}</p>
             </li>
-            <li /*onClick={() => navigate("/printing/bulkprint")}*/ className="disabled">
-            	<h1 className="maker"><span>{t('page.printing.bulkprint.title', "Bulk Print")}</span></h1>
-              <p>{t('page.printing.bulkprint.baseDescription', "Batch print labels for parts.")}</p>
-            </li>
+            <Popup 
+              hoverable
+              content={<p>
+                <Trans i18nKey="popup.makerSubscriptionRequired">
+                  Requires a Maker level subscription. <a href="https://binner.io/signup" target="_blank" rel="noreferrer">Click here</a> for details.
+                </Trans></p>} 
+              trigger={<li /*onClick={() => navigate("/printing/bulkprint")}*/ className="disabled">
+            	  <h1 className="maker"><span>{t('page.printing.bulkprint.title', "Bulk Print")}</span></h1>
+                <p>{t('page.printing.bulkprint.baseDescription', "Batch print labels for parts.")}</p>
+              </li>} 
+            />
           </ul>
         </div>
       </Segment>
