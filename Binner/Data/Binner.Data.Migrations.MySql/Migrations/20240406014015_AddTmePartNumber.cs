0be5f890219f40f44f7134ad273d3cb772e7fe1d
Michael <mikeb@360replays.com>
2024-04-05 18:48:29 -0700

    Adds some fixes to previewing custom labels in the label editor. Adds Tme part number schema.

---
+++ Binner/Data/Binner.Data.Migrations.MySql/Migrations/20240406014015_AddTmePartNumber.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Binner.Data.Migrations.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTmePartNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TmePartNumber",
+                schema: "dbo",
+                table: "Parts",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TmePartNumber",
+                schema: "dbo",
+                table: "Parts");
+        }
+    }
+}
---
+++ Binner/Data/Binner.Data.Migrations.Sqlite/Migrations/20240406013550_AddTmePartNumber.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Binner.Data.Migrations.Sqlite.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTmePartNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TmePartNumber",
+                schema: "dbo",
+                table: "Parts",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TmePartNumber",
+                schema: "dbo",
+                table: "Parts");
+        }
+    }
+}
