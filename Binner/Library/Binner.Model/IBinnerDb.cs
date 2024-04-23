ad9ce9489560b99aa62c918b6ce3307b5325b584
Michael <mikeb@360replays.com>
2024-04-22 20:02:09 -0700

    Adds more unit tests (many more to come)

--- Binner/Library/Binner.Model/IBinnerDb.cs
+++ Binner/Library/Binner.Model/IBinnerDb.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace Binner.Model
+﻿namespace Binner.Model
 {
     public interface IBinnerDb
     {
---
+++ Binner/Tests/Binner.Common.Tests/IO/IPAddressExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using Binner.Common.Extensions;
+using NUnit.Framework;
+using System.Net;
+
+namespace Binner.Common.Tests.IO
+{
+    [TestFixture]
+    public class IPAddressExtensionsTests
+    {
+        [TestCase("0.0.0.0", ExpectedResult = 0U, Description = "0.0.0.0")]
+        [TestCase("127.0.0.1", ExpectedResult = 2130706433U, Description = "127.0.0.1")]
+        [TestCase("192.168.1.55", ExpectedResult = 3232235831U, Description = "192.168.1.55")]
+        [TestCase("54.22.161.99", ExpectedResult = 907452771U, Description = "54.22.161.99")]
+        [TestCase("12.24.36.48", ExpectedResult = 202908720U, Description = "12.24.36.48")]
+        [TestCase("1.1.1.1", ExpectedResult = 16843009U, Description = "1.1.1.1")]
+        [TestCase("255.255.255.255", ExpectedResult = 4294967295U, Description = "255.255.255.255")]
+        [Test]
+        public uint ShouldIpToInt(string ipAddressStr)
+        {
+            var ipAddress = IPAddress.Parse(ipAddressStr);
+            var ip = ipAddress.ToUInt();
+            return ip;
+        }
+
+        [TestCase(0U, ExpectedResult = "0.0.0.0")]
+        [TestCase(2130706433U, ExpectedResult = "127.0.0.1")]
+        [TestCase(3232235831U, ExpectedResult = "192.168.1.55")]
+        [TestCase(907452771U, ExpectedResult = "54.22.161.99")]
+        [TestCase(202908720U, ExpectedResult = "12.24.36.48")]
+        [TestCase(16843009U, ExpectedResult = "1.1.1.1")]
+        [TestCase(4294967295U, ExpectedResult = "255.255.255.255")]
+        [Test]
+        public string ShouldIntToIp(uint ip)
+        {
+            var ipAddress = ip.ToIpAddress();
+            return ipAddress.ToString();
+        }
+    }
+}
