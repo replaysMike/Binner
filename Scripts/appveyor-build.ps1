985263ec325cbc213fad56291d81d2227bb80a26
Michael <mikeb@360replays.com>
2025-02-20 03:52:25 -0800

    updated licensing version. added build time elapsed metrics

--- Scripts/appveyor-build.ps1
+++ Scripts/appveyor-build.ps1
@@ -38,44 +38,57 @@ Write-Host "Publishing for each environment..." -ForegroundColor green
 
 $buildEnv = "win-x64"
 Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
-dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+Measure-Command {
+  dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+}
 
 Write-Host "Building the UI..." -ForegroundColor cyan
-cd .\Binner\Binner.Web\ClientApp
-npm install
-Write-Host "npm exit code: $LASTEXITCODE"
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
-npm run build
-Write-Host "npm exit code: $LASTEXITCODE"
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
-cd ..\..\..\
+Measure-Command {
+  cd .\Binner\Binner.Web\ClientApp
+  npm install
+  Write-Host "npm exit code: $LASTEXITCODE"
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+  npm run build
+  Write-Host "npm exit code: $LASTEXITCODE"
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+  cd ..\..\..\
 
-robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+  robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+}
+
 
 $buildEnv = "linux-x64"
 Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
-dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
-robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+Measure-Command {
+  dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+  robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+}
 
 $buildEnv = "linux-arm"
 Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
-dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
-robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+Measure-Command {
+  dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+  robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+}
 
 $buildEnv = "linux-arm64"
 Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
-dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
-robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+Measure-Command {
+  dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+  robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+}
 
 $buildEnv = "osx-x64"
 Write-Host "Publishing for $buildEnv..." -ForegroundColor cyan
-dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
-robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+Measure-Command {
+  dotnet publish $project -r $buildEnv -c $releaseConfiguration --self-contained
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+  robocopy .\Binner\Binner.Web\ClientApp\build .\Binner\Binner.Web\bin\$releaseConfiguration\$framework\$buildEnv\publish\ClientApp\build /s
+}
 
 # transform configurations per environment
 # windows does not need a transform
@@ -114,30 +127,35 @@ if (!$?) { exit -1  }
 
 # build installers
 Write-Host "Building Installers" -ForegroundColor green
-Set-Location -Path .\Binner\BinnerInstaller
-(Get-Content .\BinnerInstaller.iss).replace('MyAppVersion "0.0"', 'MyAppVersion "' + $env:APPVEYOR_BUILD_VERSION + '"') | Set-Content .\BinnerInstaller.iss
-Write-Host "Building installer using the following script:" -ForegroundColor cyan
-cat .\BinnerInstaller.iss
-.\build-installer.cmd
-if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
-
+Measure-Command {
+  Set-Location -Path .\Binner\BinnerInstaller
+  (Get-Content .\BinnerInstaller.iss).replace('MyAppVersion "0.0"', 'MyAppVersion "' + $env:APPVEYOR_BUILD_VERSION + '"') | Set-Content .\BinnerInstaller.iss
+  Write-Host "Building installer using the following script:" -ForegroundColor cyan
+  cat .\BinnerInstaller.iss
+  .\build-installer.cmd
+  if ($LastExitCode -ne 0) { exit $LASTEXITCODE }
+}
 
 # package artifacts
 Write-Host "Creating artifact archives..." -ForegroundColor green
-Set-Location -Path ..\..\
+Measure-Command {
+  Set-Location -Path ..\..\
 
-# (note) windows doesn't need this, it has it's own installer that won't be archived
+  # (note) windows doesn't need this, it has it's own installer that won't be archived
 
-tar -czf Binner_linux-x64.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\linux-x64\publish .
+  tar -czf Binner_linux-x64.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\linux-x64\publish .
 
-tar -czf Binner_linux-arm.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\linux-arm\publish .
+  tar -czf Binner_linux-arm.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\linux-arm\publish .
 
-tar -czf Binner_linux-arm64.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\linux-arm64\publish .
+  tar -czf Binner_linux-arm64.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\linux-arm64\publish .
 
-tar -czf Binner_osx-x64.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\osx-x64\publish .
+  tar -czf Binner_osx-x64.targz -C .\Binner\Binner.Web\bin\$($releaseConfiguration)\$framework\osx-x64\publish .
+}
 
-Write-Host "Uploading Artifacts" -ForegroundColor green
-Get-ChildItem .\Binner\BinnerInstaller\*.exe | % { Push-AppveyorArtifact $_.FullName }
+Measure-Command {
+  Write-Host "Uploading Artifacts" -ForegroundColor green
+  Get-ChildItem .\Binner\BinnerInstaller\*.exe | % { Push-AppveyorArtifact $_.FullName }
 
-# rename these artifacts to include the build version number
-Get-ChildItem .\*.targz -recurse | % { Push-AppveyorArtifact $_.FullName -FileName "$($_.Basename)-$env:APPVEYOR_BUILD_VERSION.tar.gz" }
+  # rename these artifacts to include the build version number
+  Get-ChildItem .\*.targz -recurse | % { Push-AppveyorArtifact $_.FullName -FileName "$($_.Basename)-$env:APPVEYOR_BUILD_VERSION.tar.gz" }
+}
