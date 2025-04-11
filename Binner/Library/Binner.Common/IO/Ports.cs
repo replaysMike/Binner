8a18e16a17b6f13d24f4af36492a184bbc5e4b67
Michael Brown <mbrown@provantagex.com>
2025-04-11 09:32:29 -0700

    adds error level output to console

---
+++ Binner/Library/Binner.Common/IO/Ports.cs
@@ -0,0 +1,27 @@
+﻿namespace Binner.Common.IO
+{
+    public static class Ports
+    {
+        /// <summary>
+        /// Check if a TCP port is currently in use
+        /// </summary>
+        /// <param name="port"></param>
+        /// <returns></returns>
+        public static bool IsPortInUse(int port)
+        {
+            try
+            {
+                using (var tcpListener = new System.Net.Sockets.TcpListener(System.Net.IPAddress.Any, port))
+                {
+                    tcpListener.Start();
+                    tcpListener.Stop();
+                }
+                return false;
+            }
+            catch (System.Net.Sockets.SocketException)
+            {
+                return true;
+            }
+        }
+    }
+}
