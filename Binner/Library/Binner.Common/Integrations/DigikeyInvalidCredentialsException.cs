411c7ae9db7e4088d0e1e240d888ade269a4ebbe
Michael <mikeb@360replays.com>
2025-05-01 12:14:23 -0700

    adds support for invalid client_id message

---
+++ Binner/Library/Binner.Common/Integrations/DigikeyInvalidCredentialsException.cs
@@ -0,0 +1,23 @@
+﻿using Binner.Model;
+using Binner.Model.Integrations.DigiKey;
+using System;
+
+namespace Binner.Common.Integrations
+{
+    /// <summary>
+    /// Occurs when the Digikey api ClientId is invalid
+    /// </summary>
+    public class DigikeyInvalidCredentialsException : Exception
+    {
+        public OAuthAuthorization Authorization { get; }
+        public DigiKeyApiVersion ApiVersion { get; }
+        public string? ErrorResponseVersion { get; }
+
+        public DigikeyInvalidCredentialsException(OAuthAuthorization authorization, DigiKeyApiVersion apiVersion, string? errorResponseVersion, string message) : base(message)
+        {
+            Authorization = authorization;
+            ApiVersion = apiVersion;
+            ErrorResponseVersion = errorResponseVersion;
+        }
+    }
+}
