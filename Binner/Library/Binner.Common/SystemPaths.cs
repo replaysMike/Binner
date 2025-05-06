86409b1a7442c98b5739681dcfbbc33d4be8d4e8
Michael <mikeb@360replays.com>
2025-05-05 18:27:33 -0700

    Fixes a bug with DigiKey v4 api responses sometimes returns an url without protocol specified. Eg. "//digikey.com/products/LM358" instead of "https://digikey.com/products/LM358"

--- Binner/Library/Binner.Common/SystemPaths.cs
+++ Binner/Library/Binner.Common/SystemPaths.cs
@@ -22,5 +22,26 @@ namespace Binner.Common
                 return string.IsNullOrEmpty(serviceConfig.SslCertificate) ? AppConstants.DefaultWindowsCertificatesPath : serviceConfig.SslCertificate;
             return string.IsNullOrEmpty(serviceConfig.SslCertificate) ? AppConstants.DefaultCertificatesPath : serviceConfig.SslCertificate;
         }
+
+        /// <summary>
+        /// Ensure that a the url passed is a valid http/https formatted url
+        /// </summary>
+        /// <param name="url">A potential website url</param>
+        /// <returns>null if invalid</returns>
+        public static string? EnsureValidAbsoluteHttpUrl(string? url)
+        {
+            if (string.IsNullOrEmpty(url)) return url;
+
+            try
+            {
+                if (url.StartsWith("//")) url = "https:" + url;
+                if (!url.StartsWith("http:") && !url.StartsWith("https:")) url = "https://" + url;
+                var uri = new Uri(url);
+
+                return uri.ToString();
+            }
+            catch (UriFormatException) { }
+            return null;
+        }
     }
 }
