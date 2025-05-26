772bfc2e011ff5c0f997977b90a970b657cb10cd
Michael <mikeb@360replays.com>
2025-05-26 13:21:02 -0700

    Fixes #381 - Custom field layout not wrapping properly with more than 4 fields in Inventory page

--- Binner/Binner.Web/ClientApp/src/components/CustomFieldValues.js
+++ Binner/Binner.Web/ClientApp/src/components/CustomFieldValues.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo, useCallback } from "react";
+import React from "react";
 import { Form, Popup, Header } from "semantic-ui-react";
 import PropTypes from "prop-types";
 import _ from "underscore";
