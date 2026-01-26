3ff229380617c30a71ddb1962e9682d012a730c5
Michael <mikeb@360replays.com>
2026-01-26 12:41:05 -0800

    adds better support for detecting led colors. added missing array part types. added detection of inductor values. fixed detection of capacitor values.

--- Binner/Binner.Web/ClientApp/src/common/formatting.js
+++ Binner/Binner.Web/ClientApp/src/common/formatting.js
@@ -1,5 +1,6 @@
 export const resistorTerminators = ['o', 'k', 'K', 'n', 'm', 'M', 'g', 'G', 'Ω'];
-export const capacitorTerminators = ['F', 'mF', 'mf', 'uF', 'uf', 'μf', 'μF', 'nF', 'nf', 'pF', 'pf'];
+export const capacitorTerminators = ['F', 'f', 'mF', 'mf', 'uF', 'uf', 'μf', 'μF', 'UF', 'nF', 'nf', 'pF', 'pf'];
+export const inductorTerminators = ['H', 'h', 'mH', 'mh', 'uh', 'uH', 'µH', 'UH', 'nH', 'nh', 'pH', 'ph'];
 
 /**
  * Format a value as a resistor value
@@ -50,34 +51,85 @@ export const formatCapacitorValue = (value) => {
     const terminator = terminators[t];
     if (value.endsWith(terminator) && !isNaN(value.substr(0, value.length - terminator.length))) {
       switch (terminator) {
+        case 'f':
+          return value.replace('f', 'F'); // farad
         case 'F':
-          return value;
+          return value; // farad
         case 'mF':
-          return value;
+          return value; // milifarad
         case 'mf':
-          return value = value.replace('mf', 'mF');
+          return value = value.replace('mf', 'mF'); // milifarad
         case 'uF':
-          return value = value.replace('uF', 'μF');
+          return value = value.replace('uF', 'μF'); // microfarad
         case 'uf':
-          return value = value.replace('uf', 'μF');
+          return value = value.replace('uf', 'μF'); // microfarad
+        case 'UF':
+          return value = value.replace('UF', 'μF'); // microfarad
         case 'μf':
-          return value = value.replace('μf', 'μF');
+          return value = value.replace('μf', 'μF'); // microfarad
         case 'μF':
-          return value;
+          return value; // microfarad
         case 'nF':
-          return value;
+          return value; // nanofarad
         case 'nf':
-          return value = value.replace('nf', 'nF');
+          return value = value.replace('nf', 'nF'); // nanofarad
         case 'pF':
-          return value;
+          return value; // picofarad
         case 'pf':
-          return value = value.replace('pf', 'Pf');
+          return value = value.replace('pf', 'pF'); // picofarad
       }
     }
   }
-  // no terminator provided, if it's a number append the ohms symbol
+  // no terminator provided, if it's a number append the symbol
   if (!isNaN(value))
-    return value + 'μf';
+    return value + 'μF'; // microfarad
+  // not a number, return original value
+  return value;
+};
+
+/**
+ * Format a value as a inductor value
+ * @param {string} value value to format
+ * @returns formatted inductor value
+ */
+export const formatInductorValue = (value) => {
+  const terminators = inductorTerminators;
+  for (let t = 0; t < terminators.length; t++) {
+    const terminator = terminators[t];
+    if (value.endsWith(terminator) && !isNaN(value.substr(0, value.length - terminator.length))) {
+      switch (terminator) {
+        case 'h':
+          return value.replace('h', 'H'); // henry
+        case 'H':
+          return value; // henry
+        case 'mh':
+          return value.replace('mh', 'mH'); // millahenry
+        case 'mH':
+          return value; // millahenry
+        case 'uH':
+          return value = value.replace('uH', 'μH'); // microhenry
+        case 'uh':
+          return value = value.replace('uh', 'μH'); // microhenry
+        case 'UH':
+          return value = value.replace('UH', 'μH'); // microhenry
+        case 'μh':
+          return value = value.replace('μh', 'μH'); // microhenry
+        case 'μH':
+          return value; // microhenry
+        case 'nH':
+          return value; // nanohenry
+        case 'nh':
+          return value = value.replace('nh', 'nH'); // nanohenry
+        case 'pH':
+          return value; // picohenry
+        case 'ph':
+          return value = value.replace('ph', 'pH'); // picohenry
+      }
+    }
+  }
+  // no terminator provided, if it's a number append the symbol
+  if (!isNaN(value))
+    return value + 'μH'; // microhenry
   // not a number, return original value
   return value;
 };
