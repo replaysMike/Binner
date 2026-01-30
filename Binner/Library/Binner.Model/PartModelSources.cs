6a0fa5187b89843da98a7695e22eb18a46181c6f
Michael <mikeb@360replays.com>
2026-01-30 15:28:41 -0800

    Updated AI data source models. Upgraded ImageSharp to latest version to get rid of vulnerability report.

--- Binner/Library/Binner.Model/PartModelSources.cs
+++ Binner/Library/Binner.Model/PartModelSources.cs
@@ -7,7 +7,14 @@
     {
         UserUpload = 0,
         Binner,
+        // https://ultralibrarian.com
         UltraLibrarian,
+        // https://snapmagic.com, https://snapeda.com
         SnapMagic,
+        // https://componentsearchengine.com
+        ComponentSearchEngine,
+        // https://3dfindit.com
+        ThreeDFindIt,
+        Other
     }
 }
---
+++ Binner/Library/Binner.Model/PinoutDefinition.cs
@@ -0,0 +1,41 @@
+﻿namespace Binner.Model
+{
+    /// <summary>
+    /// Pinout definition, serialized as json
+    /// </summary>
+    public class PinoutDefinition
+    {
+        /// <summary>
+        /// List of pins in the pinout
+        /// </summary>
+        public List<PinDescription> Pins { get; set; } = new();
+    }
+
+    public class PinDescription
+    {
+        /// <summary>
+        /// Pin number, 1-based
+        /// </summary>
+        public int Pin { get; set; }
+
+        /// <summary>
+        /// Label that describes the pin's purpose, e.g. "Vout", "GND", "Vs", etc.
+        /// </summary>
+        public string Label { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The pin's function, e.g. "Temperature Sensor Analog Output"
+        /// </summary>
+        public string? Function { get; set; }
+
+        /// <summary>
+        /// The type of pin, e.g. "Input", "Output", "Bidirectional", "Power", "Ground", etc.
+        /// </summary>
+        public string? Type { get; set; }
+
+        /// <summary>
+        /// Description of the pin such as "Supply input; connect to 4V-30V"
+        /// </summary>
+        public string? Description { get; set; }
+    }
+}
