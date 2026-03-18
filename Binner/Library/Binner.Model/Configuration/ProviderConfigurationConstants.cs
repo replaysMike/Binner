225ffabfcbb680fa2b8da31a55308dedc4dcb9a4
Michael <mikeb@360replays.com>
2026-03-18 11:23:00 -0700

    Fixes #465 - Env var handling Environment variable handling for database parameters had some errors in the implementation.

---
+++ Binner/Library/Binner.Model/Configuration/ProviderConfigurationConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace Binner.Model.Configuration
+{
+    /// <summary>
+    /// Constant name values used to build connection strings
+    /// </summary>
+    public class ProviderConfigurationConstants
+    {
+        public const string ConnectionString = "ConnectionString";
+        public const string Host = "Host";
+        public const string Port = "Port";
+        public const string Database = "Database";
+        public const string Username = "Username";
+        public const string Password = "Password";
+        public const string SslMode = "SslMode";
+        public const string AdditionalParameters = "AdditionalParameters";
+        public const string Filename = "Filename";
+    }
+}
